16e071f70f5d7cf70817124906dfc43594991e8c
Xico <franciscojr96@hotmail.com>
2020-12-14 00:58:18 -0300

    created client

--- client.js
+++ client.js
@@ -1,2 +1,13 @@
-const sequlize = require('sequelize');
+const Sequlize = require('sequelize');
 
+const connect = Sequelize({
+    dialect: 'sqlite',
+    storage: 'Banco_Biblioteca.sdb'
+  });
+
+  try {
+    connect.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
