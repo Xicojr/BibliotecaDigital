43f4cbd0459862d7e839373751026353b3924f8b
Xico <franciscojr96@hotmail.com>
2020-12-15 21:33:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 //Conexão ao Banco de Dados
 const sqlite3 = require('sqlite3').verbose();
